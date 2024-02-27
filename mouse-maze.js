f3f1d04c45eb3e6cc77db94a51124687f75fbece
Chris Maruca <chrismaruca@ucla.edu>
2024-02-27 15:52:44 -0800

    Add randomized maze generator

--- mouse-maze.js
+++ mouse-maze.js
@@ -24,6 +24,104 @@ export class Mouse_Maze extends Scene {
         this.N = 12; // Dimensions: 12 x 12
         this.SIZE = this.N * 4;
     }
+        
+    // Returns number between 0 to max-1
+    get_rand_num(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    // Use randomized recursive DFS to generate the maze definition
+    recursive_maze_gen(Maze, x, z) {
+        let neighbor_checked = [false, false, false, false];
+
+        for (let i = 0; i < 4; i++) {
+            let r = this.get_rand_num(4);
+
+            while (neighbor_checked[r]) {
+                r = (r + 1) % 4;
+            }
+            neighbor_checked[r] = true;
+            let nx = x, nz = z;
+
+            switch(r) {
+                case 0:
+                    nx += 1;
+                    if (nx >= this.N) {
+                        continue;
+                    }
+                    break;
+                case 1:
+                    nz += 1;
+                    if (nz >= this.N) {
+                        continue;
+                    }
+                    break;
+                case 2:
+                    nx -= 1;
+                    if (nx < 0) {
+                        continue;
+                    }
+                    break;
+                case 3:
+                    nz -= 1;
+                    if (nz < 0) {
+                        continue;
+                    }
+                    break;
+            }
+
+            if (nx >= 0 && nx < this.N && nz >= 0 && nz < this.N && !Maze.visited[nx][nz]) {
+                Maze.visited[nx][nz] = true;
+                Maze.connected[x][z][r] = true;
+                Maze.connected[nx][nz][(r+2)%4] = true;
+                this.recursive_maze_gen(Maze, nx, nz);
+            }
+        }
+    }
+
+    // Create a randomized maze
+    generate_maze_connections() {
+        let n = this.N;
+        let Maze = {
+            visited: new Array(n).fill(0).map(() => new Array(n).fill(false)),
+            connected: new Array(n).fill(0).map(() => new Array(n).fill(0).map(() => new Array(4).fill(false)))
+        }
+        // directions: 0 == +x, 1 == +z, 2 == -x, 3 == -z
+        Maze.visited[0][0] = true;
+        this.recursive_maze_gen(Maze, 0, 0);
+        return Maze.connected;
+    }
+
+    log_maze(connected) {
+        let out = "";
+        for (let i = 0; i < this.N; i++) {
+            for (let j = 0; j < this.N; j++) {
+                out += "+";
+                out += (connected[i][j][2] ? " " : "-");
+                //out += "+";
+            }
+            out += "+\n";
+            for (let j = 0; j < this.N; j++) {
+                out += (connected[i][j][3] ? " " : "|");
+                out += " ";
+                //out += (connected[i][j][1] ? " " : "|");
+            }
+            /*
+            out += "\n";
+            for (let j = 0; j < this.N; j++) {
+                out += "+";
+                out += (connected[i][j][0] ? " " : "-");
+                out += "+";
+            }*/
+            out += "|\n";
+        }
+        for (let i = 0; i < this.N; i++) {
+            out += "+-";
+        }
+        out += "+\n";
+        console.log(out);
+    }
+
 
     draw_floor(context, program_state) {
         let floor_model_transform = Mat4.identity();
@@ -63,6 +161,8 @@ export class Mouse_Maze extends Scene {
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             program_state.set_camera(Mat4.translation(0, 0, 0));
+            
+            this.log_maze(this.generate_maze_connections());
         }
         // Projection matrix
         program_state.projection_transform = Mat4.perspective(
